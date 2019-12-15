208a7da997bb028ffd8161f9c481c9555278999e
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2019-12-15 11:52:44 -0500

    loading in scroll magic

---
+++ scroll.js
@@ -0,0 +1,13 @@
+const root = document.querySelector('.roots')
+
+console.log(root)
+
+const controller = new ScrollMagic.Controller()
+
+// const scene = new ScrollMagic.scene({triggerElement: })
+/* 
+root
+  water
+  power
+  chlora
+*/
